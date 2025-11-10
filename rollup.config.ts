14902d63313efe5132d64b662343dcd1b87400ce
MtSatou <2185066282@qq.com>
2025-11-10 11:40:14 +0800

    优化rollup打包行为

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,20 +2,59 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import json from '@rollup/plugin-json';
+import terser from '@rollup/plugin-terser';
+import copy from 'rollup-plugin-copy';
 
 export default {
-  input: './src/index.ts',                              // 入口文件
+  input: './src/index.ts',
   output: {
-    file: './dist/bundle.js',                           // 打包后的输出文件
-    format: 'cjs',                                      // 输出格式为 CommonJS
-    sourcemap: true,                                    // 启用 sourcemap 调试
+    dir: './dist',
+    format: 'cjs',
+    sourcemap: false,
+    preserveModules: true,
   },
   plugins: [
     resolve({
-      preferBuiltins: true,                             // 使用内建模块
-    }),                                                 // 解析 Node 模块
-    commonjs(),                                         // 将 CommonJS 模块转换为 ES6
-    typescript({ tsconfig: './tsconfig.json' }),        // 使用 TypeScript 插件
-    json(),                                             // 处理 JSON 文件导入
+      preferBuiltins: true,
+    }),
+    // 将 CommonJS 模块转换为 ES6
+    commonjs({
+      requireReturnsDefault: 'preferred',
+      defaultIsModuleExports: true,
+    }),
+    typescript(
+      {
+        tsconfig: './tsconfig.json',
+        tsconfigOverride: {
+          compilerOptions: {
+            target: 'ES2020',
+            module: 'ESNext',
+            moduleResolution: 'Node',
+          },
+        },
+        clean: true,
+      }),
+    // 处理 JSON 文件导入
+    json(),
+    // 压缩代码
+    terser({
+      // 混淆变量名
+      mangle: true,
+      format: {
+        // 移除注释
+        comments: false,
+      },
+
+    }),
+    // 复制文件
+    copy({
+      hook: 'writeBundle',
+      copyOnce: false,
+      verbose: true,
+      targets: [
+        { src: 'src/env/**/*', dest: 'dist/env' },
+        { src: 'LICENSE', dest: 'dist' },
+      ],
+    }),
   ],
 };
